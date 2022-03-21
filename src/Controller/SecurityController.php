350cb04fe4142c24f61213db8f0bb489831fa333
Romain <contact@rominfo.fr>
2022-03-21 13:31:18 +0100

    sf3 to sf5

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -19,23 +19,6 @@ class SecurityController extends AbstractController
         $this->security = $security;
     }
 
-    /**
-     * @Route("/login", name="app_login")
-     */
-    public function login(AuthenticationUtils $authenticationUtils, Referer $referer): Response
-    {
-        if ($this->security->isGranted('IS_AUTHENTICATED_FULLY')) {
-            return $referer->goTo();
-        }
-        $referer->set();
-
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
-    }
 
     /**
      * @Route("/login_check", name="login_check")
@@ -51,5 +34,26 @@ class SecurityController extends AbstractController
     public function logoutCheck()
     {
         // This code is never executed.
+    }
+
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
